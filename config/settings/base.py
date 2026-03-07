de9b933a715f40d03d664768639d910cfafd34d0
Enrique Gálvez <kikega@telefonica.net>
2026-03-07 02:40:26 +0100

    Añadir a setting la variable STATIC_FILES

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
